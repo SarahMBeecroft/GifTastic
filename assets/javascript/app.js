d850417f6c6d5dd23fb23090ef6d501d796ec66a
Sarah B <smblair87@gmail.com>
2019-03-28 20:56:07 -0700

    Added base template for HTML and psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+// GIPHY API KEY
+// Api Key:
+// TSAKD90Tb0AsiNIZEV1JwaX38350C95i
+
+
+// Array of topics for reactions gifs
+var topics= ["happy", "sad", "excited", "angry", "nervous", "embarrassed", "amused", "bored", "shocked", "sleepy", "confused", "frustrated", "affectionate", "proud"]
+
+// Pseudocode
+
+// On load, elements that are visible include pre-loaded buttons and area for user to submit their own button
+
+// Click event when user clicks on any of the pre-loaded buttons
+// 10 gifs tagged with word load in their static state
+// User can click gif to animate and then click to pause again, and so on
+// Gif rating will load above gif
+
+// Behind the scenes
+
+// Giphy Parameters
+// 'q'
+// 'limit'
+// 'rating' 
+
+// Create array of topics
+
+// Take topics in array and create buttons in HTML (use for loop to append button for each string in array)
+
+// Generate 10 static gifs
+
+// Add links with pause and unpaused states
+
+// Grab gif rating
+
+// Append gif and rating to page
+
+// Create submit button to add new topic, add to array, and append to page
+
+
