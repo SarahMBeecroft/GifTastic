9c1725bb625ffed94dbcdba35d391feaccfe9b0d
Sarah B <smblair87@gmail.com>
2019-03-31 16:58:34 -0700

    Got gifs to load from Giphy API and set up for pause/play

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,123 +5,106 @@
 $(document).ready(function () {
 
     // Array of topics for reactions gifs
-    var emotions = ["happy", "sad", "excited", "angry", "nervous", "embarrassed", "amused", "bored", "shocked", "sleepy"];
-    // Empty variable for topic
+    var topics = ["happy", "sad", "excited", "angry", "nervous", "embarrassed", "amused", "bored", "shocked", "sleepy"];
 
+    // Function to display topics
+    function displayTopics() {
+
+        // Declares variable for queryURL
+        var topic = $(this).attr("data-topic");
+        console.log(topic);
+
+        // Query URL for Giphy
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=TSAKD90Tb0AsiNIZEV1JwaX38350C95i&q=" + topic + "&limit=10&offset=0&rating=G&lang=en";
+        console.log(queryURL);
+
+        // Ajax call 
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+
+        }).then(function (response) {
+
+            // Testing response to make sure object is there
+            console.log(response);
+
+            // Sets up variable for response data
+            var results = response.data;
+
+            // Empties div so gifs don't stack 
+            $("#gifs").empty();
+
+            // For loop to display gifs
+            for (var i = 0; i < results.length; i++) {
+
+                // Creates variables
+                var gifDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating);
+                var gif = $("<img>");
+
+                // Pause and play attributes
+                gif.attr("src", results[i].images.original_still.url);
+                gif.attr("data-still", results[i].images.original_still.url);
+                gif.attr("data-animate", results[i].images.original.url);
+                gif.attr("data-state", "still");
+                gif.attr("class", "gif");
+
+                // Appends rating and gif to gif div
+                gifDiv.append(p);
+                gifDiv.append(gif);
+
+                // Adds both rating and gif to gif div in HTML
+                $("#gifs").append(gifDiv);
+
+            };
+        });
+    };
+
+    // Function for displaying buttons
     function makeButtons() {
 
         // Deletes button contents to avoid having repeats
         $("#buttons").empty();
 
         // For loop to generate buttons for each item in emotions array
-        for (var i = 0; i < emotions.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
             // Dynamically generates buttons for each movie in array
-            var a = $("<button>");
+            var button = $("<button>");
             // Adds class of topic-btn to each button
-            a.addClass("topic-btn");
+            button.addClass("topic-btn");
             // Adds data attribute
-            a.attr("data-name", emotions[i]);
+            button.attr("data-topic", topics[i]);
             // Provides button text
-            a.text(emotions[i]);
+            button.text(topics[i]);
             // Adds buttons to #buttons div
-            $("#buttons").append(a, " "); // Extra quotes adds space between buttons 
-        }
-    }
+            $("#buttons").append(button, " "); // Extra quotes add space between buttons 
+        };
+    };
 
-    // Calls makeButtons function so they display on page
+
+    // Function to pause and play gifs
+    function playGifs() {
+
+        var dataState = $(this).attr("data-state");
+
+        if (dataState === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        };
+    };
+
+    // Click event for topic buttons
+    $(document).on("click", ".topic-btn", displayTopics);
+
+    // Displays buttons on page
     makeButtons();
- 
-    // Click event for buttons
-    $(this).click(function () {
-        console.log(this);
 
-        // Declares variable for queryURL
-        var topic = $(this).attr("data-name");
+    // Click event to pause and play gifs
+    $(document).on("click", ".gif", playGifs);
 
-        // Query URL for Giphy
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=TSAKD90Tb0AsiNIZEV1JwaX38350C95i&limit=10&rating=pg";
-      
-        // Ajax call for button being clicked
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-
-        }).then(function (response) {
-        
-        // Creates a div to hold the gifs
-        var gifDiv = $("<div class='gifs'>");
-
-        // Stores the rating data
-        var rating = response.rating;
-     
-        // Creating an element to have the rating displayed
-        var displayRating = $("<p>").text("Rating: " + rating);
-     
-        // Displays the rating
-        gifDiv.append(displayRating);
-     
-        // Pulls the url for the gif
-        var gifURL = response.image;
-     
-        // Creating an element to hold the image
-        var image = $("<img>").attr("src", gifURL);
-     
-        // Appending the image
-        gifDiv.append(image);
-     
-        // Putting the entire movie above the previous movies
-        $("#gifs").prepend(gifDiv);
-                    
-                
-            
-        });
-    
-
-
-
-    });
-
-    // On click event for when button is clicked
-    $("#textbox").on("click", function (event) {
-        // event.preventDefault();not sure if needed 
-        // Grabs user's input from textbox
-        var topic = $("#textbox").val().trim(); // Trim will get rid of any white space before/after user input
-        // Adds movie from text box to the emotions array
-        emotions.push(topic);
-        // Calls make button function
-        makeButton();
-
-
-        // Come back to this 
-        function submitButton() {
-
-        }
-    // On load, elements that are visible include pre-loaded buttons and area for user to submit their own button
-
-    // Click event when user clicks on any of the pre-loaded buttons
-    // 10 gifs tagged with word load in their static state
-    // User can click gif to animate and then click to pause again, and so on
-    // Gif rating will load above gif
-
-    // Behind the scenes
-
-    // Giphy Parameters
-    // 'q'
-    // 'limit'
-    // 'rating' 
-
-    // Create array of topics
-
-    // Take topics in array and create buttons in HTML (use for loop to append button for each string in array)
-
-    // Generate 10 static gifs
-
-    // Add links with pause and unpaused states
-
-    // Grab gif rating
-
-    // Append gif and rating to page
-
-    // Create submit button to add new topic, add to array, and append to page
-    });
 });
+
