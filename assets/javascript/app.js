c3dee018c91082a9e6d9d6c10fb0d12d4d31f6c1
Sarah B <smblair87@gmail.com>
2019-03-30 18:40:59 -0700

    Added function to render buttons for topics array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,40 @@
 // TSAKD90Tb0AsiNIZEV1JwaX38350C95i
 
 
-// Array of topics for reactions gifs
-var topics= ["happy", "sad", "excited", "angry", "nervous", "embarrassed", "amused", "bored", "shocked", "sleepy", "confused", "frustrated", "affectionate", "proud"]
+$(document).ready(function() {
 
-// Pseudocode
+// Array of topics for reactions gifs
+var emotions = ["happy", "sad", "excited", "angry", "nervous", "embarrassed", "amused", "bored", "shocked", "sleepy"];
+// Empty variable for topic
+var emotion = "";
+
+// Query URL for Giphy
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + emotion + "&limit=10&rating=G&api_key=TSAKD90Tb0AsiNIZEV1JwaX38350C95i";
+
+
+function makeButtons () {
+
+    // Deletes button contents to avoid having repeats
+    $("#buttons").empty();
+    
+    // For loop to generate buttons for each item in emotions array
+    for (var i = 0; i < emotions.length; i++) {
+        // Dynamically generates buttons for each movie in array
+        var a = $("<button>");
+        // Adds class of emotion-btn to each button
+        a.addClass("emotion-btn");
+        // Adds data attribute
+        a.attr("data-name", emotions[i]);
+        // Provides button text
+        a.text(emotions[i]);
+        // Adds buttons to #buttons div
+        $("#buttons").append(a, " "); // Extra quotes adds space between buttons 
+    }
+    
+}
+
+// Calls make buttons function
+makeButtons();
 
 // On load, elements that are visible include pre-loaded buttons and area for user to submit their own button
 
@@ -36,4 +66,4 @@ var topics= ["happy", "sad", "excited", "angry", "nervous", "embarrassed", "amus
 
 // Create submit button to add new topic, add to array, and append to page
 
-
+})
